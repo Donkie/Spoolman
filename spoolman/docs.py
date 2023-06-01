524a8ffc42d514267d7eaf6c9d980cf00cb31e74
Donkie <daniel.cf.hultgren@gmail.com>
2023-06-01 20:03:15 +0200

    Fixed redoc title name

--- spoolman/docs.py
+++ spoolman/docs.py
@@ -47,7 +47,7 @@ def generate_docs() -> None:
         <!DOCTYPE html>
         <html>
         <head>
-        <title>Spoolson REST API v1 - ReDoc</title>
+        <title>Spoolman REST API v1 - ReDoc</title>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
