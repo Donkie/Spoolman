12b19c2c6382538bab7274c85ad8e43afe9eddbf
Donkie <daniel.cf.hultgren@gmail.com>
2023-09-16 13:54:10 +0200

    Client: Disabled vite dev https
    It's causing loading issues

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import mkcert from 'vite-plugin-mkcert'
 
 
 export default defineConfig({
-  server: { https: true },
-  plugins: [react(), svgr(), mkcert()],
+  plugins: [react(), svgr()],
 });
