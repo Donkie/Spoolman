f13c61b9e9acc033a10d2e8e074e7e2c85d3b07f
pdsccode <pdsccode@gmail.com>
2024-01-07 13:44:04 +0100

    Fixed test error that asserts 404 on create

--- tests_integration/tests/spool/test_add.py
+++ tests_integration/tests/spool/test_add.py
@@ -187,14 +187,65 @@ def test_add_spool_both_used_and_remaining_weight(random_filament: dict[str, Any
     assert result.status_code == 400  # Cannot set both used and remaining weight
 
 
-def test_add_spool_price(random_filament: dict[str, Any]):
+def test_add_spool_with_price(random_filament: dict[str, Any]):
     """Test adding a spool to the database."""
     # Execute
+    remaining_weight = 750
+    location = "The Pantry"
+    lot_nr = "123456789"
+    comment = "abcdefghåäö"
+    archived = True
+    price = 25
     result = httpx.post(
         f"{URL}/api/v1/spool",
         json={
+            "first_used": "2023-01-02T12:00:00+01:00",
+            "last_used": "2023-01-02T11:00:00Z",
             "filament_id": random_filament["id"],
-            "price": 25,
+            "remaining_weight": remaining_weight,
+            "location": location,
+            "lot_nr": lot_nr,
+            "comment": comment,
+            "archived": archived,
+            "price": price,
         },
     )
-    assert result.status_code == 400  # Cannot set price
+    result.raise_for_status()
+
+    # Verify
+    used_weight = random_filament["weight"] - remaining_weight
+    used_length = length_from_weight(
+        weight=used_weight,
+        density=random_filament["density"],
+        diameter=random_filament["diameter"],
+    )
+    remaining_length = length_from_weight(
+        weight=remaining_weight,
+        density=random_filament["density"],
+        diameter=random_filament["diameter"],
+    )
+
+    spool = result.json()
+    assert spool == {
+        "id": spool["id"],
+        "registered": spool["registered"],
+        "first_used": "2023-01-02T11:00:00Z",
+        "last_used": "2023-01-02T11:00:00Z",
+        "filament": random_filament,
+        "remaining_weight": pytest.approx(remaining_weight),
+        "used_weight": pytest.approx(used_weight),
+        "remaining_length": pytest.approx(remaining_length),
+        "used_length": pytest.approx(used_length),
+        "location": location,
+        "lot_nr": lot_nr,
+        "comment": comment,
+        "archived": archived,
+        "price": price,
+    }
+
+    # Verify that registered happened almost now (within 1 minute)
+    diff = abs((datetime.now(tz=timezone.utc) - datetime.fromisoformat(spool["registered"])).total_seconds())
+    assert diff < 60
+
+    # Clean up
+    httpx.delete(f"{URL}/api/v1/spool/{spool['id']}").raise_for_status()
