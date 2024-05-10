9ff457f2fda58ce5b2fb37fd01d252bf68dc2497
Donkie <daniel.cf.hultgren@gmail.com>
2024-05-10 12:52:41 +0200

    Updated packages

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import detector from "i18next-browser-languagedetector";
-import Backend from "i18next-xhr-backend";
+import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 import { getBasePath } from "./utils/url";
 
