7bb0c6b1ec187fb510a948ef0aba8e7d0b166836
SI Xiaolong <sixiaolonggithub@gmail.com>
2025-09-30 10:36:01 +0800

    Supports PWA.
    PWA is supported using vite-plugin-pwa.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,43 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   base: "",
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true,
+    },
+    includeAssets: ['favicon.ico', 'robots.txt'],
+    manifest: {
+      name: "Spoolman",
+      short_name: "Spoolman",
+      description: "Keep track of your inventory of 3D-printer filament spools.",
+      "icons": [
+        {
+          "purpose": "maskable",
+          "sizes": "512x512",
+          "src": "icon512_maskable.png",
+          "type": "image/png"
+        },
+        {
+          "purpose": "any",
+          "sizes": "512x512",
+          "src": "icon512_rounded.png",
+          "type": "image/png"
+        }
+      ],
+      background_color: "#1F1F1F",
+      theme_color: "#DC7734",
+      display: "standalone",
+      start_url: "/",
+      scope: "/"
+    },
+    workbox: {
+      maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+    }
+  })],
 });
