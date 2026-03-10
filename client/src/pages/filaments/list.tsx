a531321b4d86f02ae8ffba06f2b749797aa98964
akira69 <akira69@gmail.com>
2026-03-10 07:37:08 -0500

    feat(export): add separate AML/PNG label export flows and improvements

--- client/src/pages/filaments/list.tsx
+++ client/src/pages/filaments/list.tsx
@@ -1,11 +1,4 @@
-import {
-  EditOutlined,
-  EyeOutlined,
-  FileOutlined,
-  FilterOutlined,
-  PlusSquareOutlined,
-  PrinterOutlined,
-} from "@ant-design/icons";
+import { EditOutlined, EyeOutlined, FileOutlined, FilterOutlined, PlusSquareOutlined, PrinterOutlined } from "@ant-design/icons";
 import { List, useTable } from "@refinedev/antd";
 import { useInvalidate, useNavigation, useTranslate } from "@refinedev/core";
 import { Button, Dropdown, Table } from "antd";
